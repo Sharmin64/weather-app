147a8ead9fd400ddfef6fd2d76d671de54749953
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-30 13:02:34 +0600

    css implemented on html class

--- js-folder/index.js
+++ js-folder/index.js
@@ -1,9 +1,11 @@
 const apiKey = "4efa33b9995a9b5bee68489e372475b7";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4efa33b9995a9b5bee68489e372475b7";
 
 async function checkWeather() {
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
-  var data = await response.json();
+  const data = await response.json();
 
   console.log(data);
 }
+console.log("ami t o more jabo chole jabo rekhe jabo sobi");
