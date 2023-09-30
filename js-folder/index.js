fe200d7ef6f94aabdd7d0ba69b68e13d850b491c
Sharmin64 <lizaeatsbanana@gmail.com>
2023-09-30 14:44:00 +0600

    js function implemented for render data

--- js-folder/index.js
+++ js-folder/index.js
@@ -1,11 +1,29 @@
 const apiKey = "4efa33b9995a9b5bee68489e372475b7";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=4efa33b9995a9b5bee68489e372475b7";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search .btn");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   const data = await response.json();
 
   console.log(data);
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  }
+  if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
 }
-console.log("ami t o more jabo chole jabo rekhe jabo sobi");
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
